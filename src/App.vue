16cf48d50366f4770c7388adbea3f46f4551e9c6
codwerk <louis_widdy@live.fr>
2020-12-25 15:31:55 +0100

    nouveau components faqs, use v-for

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>The Vue<span>cordion</span></h1>
+    </header>
+    <section class="faqs">
+      <Faq 
+        v-for="(faq, i) in faqs" 
+        :key="i" 
+        :faq="faq" 
+        :index="i" />
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Faq from './components/faqs.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Faq
+  },
+  data() {
+    return {
+      faqs: [
+        {
+          question: "Who is the best Superhero?",
+          answer: "I'm not sure but we love him 3000",
+          open: false
+        },
+        {
+          question: "What is Goku's form called with White Hair?",
+          answer: "Mastered Ultra Instinct",
+          open: false
+        },
+        {
+          question: "Have you liked & subscried yet?",
+          answer: "YES",
+          open: false
+        }
+      ]
+    }
   }
 }
 </script>
 
-<style>
-#app {
+<style lang="scss">
+*, ::before, ::after{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #EEE;
+}
+
+header {
+  background-color: #35495E;
+  padding: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    color: #FFF;
+    font-size: 28px;
+    font-weight: 300;
+    text-transform: uppercase;
+
+    span {
+      color: #41B883;
+      font-weight: 900;
+    }
+  }
 }
 </style>
---
+++ src/components/faqs.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container_faq">
+    <div class="question">
+      {{ faq.question}}
+    </div>
+    <div class="answer">
+      {{ faq.answer}}
+    </div>
+  </div>
+</template>
+
+<script>
+ export default {
+   name: 'Faq',
+   props: ['faq', 'index'],
+   data: () => {
+     return {}
+   }
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
